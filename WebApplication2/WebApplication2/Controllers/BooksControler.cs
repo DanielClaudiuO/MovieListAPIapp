ef57dbc29f60734e57da95f02d43a33e905c0d7a
DanielClaudiuO <79627394+DanielClaudiuO@users.noreply.github.com>
2021-03-16 17:48:03 +0200

    Add files via upload

---
+++ WebApplication2/WebApplication2/Controllers/BooksControler.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksControler : ControllerBase
+    {
+       private static List<Models.Book> books = new List<Models.Book>()
+        {
+            new Models.Book() {ID=Guid.NewGuid(), Name="Permanent Record", Author="Edward Snowden", FirstEdition= 2019},
+            new Models.Book() {ID=Guid.NewGuid(), Name="Mere Christianity", Author="C.S.Lewis", FirstEdition=1952},
+            new Models.Book() {ID=Guid.NewGuid(), Name="The Fellowship of the Ring", Author="J.R.R. Tolkien", FirstEdition=1954}
+        };
+
+        [HttpGet]
+        public Models.Book[] Get()
+        {
+            return books.ToArray();
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Models.Book book)
+        {
+            if (book.ID == Guid.Empty)
+                book.ID = Guid.NewGuid();
+            books.Add(book);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] Models.Book book)
+        {
+            Models.Book book1 = books.FirstOrDefault(x => x.ID == book.ID);
+            book1.Name = book.Name;
+        }
+
+        [HttpDelete]
+        public void Delete(Guid id)
+        {
+            books.RemoveAll(book => book.ID == id);
+
+            //foreach (Models.Duckbill duckbill in duckbills)
+            //{
+            //    if (duckbill.ID == id)
+            //    {
+            //        duckbills.Remove(duckbill);
+            //    }
+            //}
+        }
+    }
+}
+
---
+++ WebApplication2/WebApplication2/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Book
+    {
+        public Guid ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+
+        public int FirstEdition { get; set; }
+    }
+}
